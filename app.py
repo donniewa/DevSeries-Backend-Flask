813c688d07d01394e00d25bacf7e0484ef25cc47
Donald White <donald.white@disney.com>
2022-09-27 17:05:13 -0700

    Added additional routes, and fixed cors

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, url_for, json, jsonify, Response
 from flask_cors import CORS
-
 from DataClass import DataClass
 
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, send_wildcard=True)
 
 my_data = DataClass()
 
@@ -17,6 +16,16 @@ def getSomeData():
 @app.route("/getSomeDataFromFile")
 def getSomeDataFromFile():
     r = my_data.getJsonFromFile('./data/hello.json')
+    return Response(json.dumps(r), mimetype='application/json')
+
+@app.route("/getSomeDataArrayFromFile")
+def getSomeDataArrayFromFile():
+    r = my_data.getJsonFromFile('./data/hello-lots.json')
+    return Response(json.dumps(r), mimetype='application/json')
+
+@app.route("/employees")
+def employees():
+    r = my_data.getJsonFromFile('./data/employees.json')
     return Response(json.dumps(r), mimetype='application/json')
 
 if __name__ == "__main__":
